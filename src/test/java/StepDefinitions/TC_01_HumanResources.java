8ec13d7d93d44b96e2f0795470f6477ab51a0576
KadirCglr <kcaglar865@gmail.com>
2023-04-15 01:08:05 +0300

    TC_01_HumanResources Delete Negatif test başarıyla test edilmiştir.

--- src/test/java/StepDefinitions/TC_01_HumanResources.java
+++ src/test/java/StepDefinitions/TC_01_HumanResources.java
@@ -2,18 +2,16 @@ package StepDefinitions;
 
 import Pages.DialogContent;
 import Pages.LeftNav;
-import io.cucumber.datatable.DataTable;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
 
 public class TC_01_HumanResources {
 
     LeftNav ln=new LeftNav();
     DialogContent dc=new DialogContent();
 
+    @Then("is no data message should be displayed")
+    public void isNoDataMessageShouldBeDisplayed() {
 
+        dc.verifyContainsTextFunction(dc.isNoData, "There is no data to display");
+    }
 }
